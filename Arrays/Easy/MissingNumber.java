451944c1eba0e72f26b5822c7fe0c5688f328d82
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2025-09-15 10:19:35 +0530

    Create MissingNumber.java

---
+++ Arrays/Easy/MissingNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // int n = nums.length;
+        // int expectedSum = n * ( n + 1) /2;
+        // int actualSum = 0;
+        // for(int num : nums) {
+        //     actualSum += num;
+        // }
+        // return expectedSum - actualSum;
+        rerurn MissingNumber(nums);
+    }
+
+    public int MissingNumber(int[] nums) {
+        int missing = nums.length;
+        for(int i=0 ; i<nums.length ; i++) {
+            missing = missing ^ i ^ nums[i];
+        }
+        return missing;
+    }
+}
