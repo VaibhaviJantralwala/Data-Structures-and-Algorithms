3ef618b2d897c752d3b480a30bd525e10ee19285
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2025-09-24 22:56:25 +0530

    Create MajorityElementII.java

---
+++ Arrays/Medium/MajorityElementII.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put( num , map.getOrDefault( num , 0 ) + 1 );
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+
+        for( int num : map.keySet() ) {
+            if( map.get(num) > n/3 ) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
+}
