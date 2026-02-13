9e5548f1082141924d4517f878795de14fbf41b3
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-02-13 12:14:52 +0530

    Update FruitIntoBasket.java

--- Arrays/Medium/FruitIntoBasket.java
+++ Arrays/Medium/FruitIntoBasket.java
@@ -1,83 +1,53 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int n = fruits.length;
-        int high = 0 , left = 0;
-        int res = 0;
 
-        Map<Integer,Integer> map = new HashMap<>();
+        // int n = fruits.length;
+        // int high = 0 , left = 0;
+        // int res = 0;
 
-        for(high = 0 ; high < n ; high++){
-            int frt = fruits[high];
-            map.put(frt , map.getOrDefault( frt , 0 ) + 1);
+        // Map<Integer,Integer> map = new HashMap<>();
 
-            while( map.size() > 2 ){
-                int leftFrt = fruits[left];
-                map.put(leftFrt , map.get(leftFrt) - 1);
+        // for(high = 0 ; high < n ; high++){
+        //     int frt = fruits[high];
+        //     map.put(frt , map.getOrDefault( frt , 0 ) + 1);
 
-                if( map.get(leftFrt) == 0 ){
-                map.remove(leftFrt);
-                }
-                left++;
+        //     while( map.size() > 2 ){
+        //         int leftFrt = fruits[left];
+        //         map.put(leftFrt , map.get(leftFrt) - 1);
+
+        //         if( map.get(leftFrt) == 0 ){
+        //         map.remove(leftFrt);
+        //         }
+        //         left++;
+        //     }
+        //     res = Math.max( res , high - left + 1);
+        // }
+        // return res;
+
+        int lastFruit = -1;
+        int secLastFruit = -1;
+        int lastFruitCount = 0;
+        int currMax = 0;
+        int max = 0;
+
+        for( int frt : fruits) {
+
+            if( frt == lastFruit || frt == secLastFruit ){
+                currMax++;
+            }else{
+                currMax = lastFruitCount + 1;
             }
-            res = Math.max( res , high - left + 1);
+
+            if( frt == lastFruit ){
+                lastFruitCount++;
+            }else{
+                lastFruitCount = 1;
+                secLastFruit = lastFruit;
+                lastFruit = frt;
+            }
+
+            max = Math.max( max , currMax );
         }
-        return res;
-    }
-}
-================================================================================
-
-class Solution {
-    public int totalFruit(int[] fruits) {
-
-        //sliding window
-        int left=0;
-        int len=fruits.length;
-        int ans =0;
-
-        //since fruits[i] can be only 1 less than the length i can have an array of size len
-        //to keep a track whether i have collected that fruit or not
-        int[] map = new int[len];
-
-        //this holds the no fruits i have seen till now if it exceeds 2
-        //then its invalid
-        int count =0;
-
-        for(int right=0; right<len; right++)
-        {
-
-            //is it the first time i see this fruit
-            if(map[fruits[right]]==0)
-            {
-                //increment the map to denote that this fruit is seen
-                map[fruits[right]]++;
-                
-                count++;
-            }
-            else
-            {   //just increment its position
-                map[fruits[right]]++;
-            }
-
-            
-
-            while(count>2) //invalid
-            {
-                map[fruits[left]]--;
-                if(map[fruits[left]] == 0)
-                {
-                    //decrement the counter
-                    count--;
-                }
-                left++;
-            }
-
-            //valid
-            ans = Math.max(ans, right-left+1);
-
-        }
-
-        return ans;
-
-        
+        return max;
     }
 }
