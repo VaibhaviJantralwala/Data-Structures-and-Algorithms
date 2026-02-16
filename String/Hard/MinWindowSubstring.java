2d1f57ed2ffc49fbf8c0416788f48b6a918d0fe4
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-02-16 13:21:01 +0530

    Update MinWindowSubstring.java

--- String/Hard/MinWindowSubstring.java
+++ String/Hard/MinWindowSubstring.java
@@ -42,5 +42,41 @@ class Solution {
             }
         }
         return minLen == Integer.MAX_VALUE ? "" : s.substring(start , start + minLen );
+        
+        // Another Solution
+
+        // if( s == null || t == null || s.length() == 0 || t.length() == 0 ){
+        //     return "";
+        // }
+
+        // int mapS[] = new int[256];
+        // int mapT[] = new int[256];
+
+        // for(char c : t.toCharArray() ) mapT[c]++;
+
+        // int left = 0 , right = 0 , minStart = 0 , minLen = Integer.MAX_VALUE;
+
+        // for(right =0 ; right < s.length() ; right++){
+
+        //     mapS[s.charAt(right)]++;
+
+        //     while( contains( mapS , mapT ) ){
+        //         if( right - left + 1 < minLen ){
+        //             minLen = right - left + 1;
+        //             minStart = left;
+        //         }
+        //         mapS[s.charAt(left++)]--;
+        //     }
+        // }
+        // return minLen == Integer.MAX_VALUE ? "" : s.substring( minStart , minStart + minLen );
+    }
+
+    private boolean contains( int[] mapS , int[] mapT ){
+        for( int i=0 ; i<256 ; i++ ){
+            if( mapT[i] > mapS[i] ){
+                return false;
+            }
+        }
+        return true;
     }
 }
