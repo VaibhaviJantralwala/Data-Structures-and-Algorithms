b41ec953b11aa04192008ed982cb5058d8262552
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-07-02 22:22:38 +0530

    Add solution for appending characters to form subsequence
    Implement method to count characters needed to append to make 't' a subsequence of 's'.

---
+++ String/Medium /AppendCharToMakeSubsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+
+        int count = 0;
+        int i=0;
+        int j=0;
+
+        while( i < s.length() && j < t.length() ){
+            if( s.charAt(i) == t.charAt(j) ){
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        count = t.length() - j;
+        
+        return count;
+    }
+}
