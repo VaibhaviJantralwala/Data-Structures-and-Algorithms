a5b13878b07cf5701c363ff0f19341a8e3043637
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-07-14 17:42:56 +0530

    Refactor removeDuplicates method
    Refactor removeDuplicates method to improve logic for removing duplicates in a sorted array.

--- Arrays/Easy/RemoveDuplictaesInSortedArray.java
+++ Arrays/Easy/RemoveDuplictaesInSortedArray.java
@@ -1,5 +1,25 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+       int off = 0;
+       int result = off + 1;
+       int cm = off + 1;
+
+       while( cm < nums.length ){
+        if( nums[cm] == nums[cm - 1] ){
+            cm++;
+            continue;
+        }
+        nums[off+1] = nums[cm];
+        off++;
+        result++;
+        cm++;
+       }
+       return result;
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
         if( nums.length == 0 ) return 0;
         int i=0 ;
         for(int j=0 ; j<nums.length ; j++){
