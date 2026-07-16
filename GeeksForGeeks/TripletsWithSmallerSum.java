29a260740f2cac17a195799f7f6a6682b31bfedf
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-07-16 13:16:55 +0530

    Update TripletsWithSmallerSum.java

--- GeeksForGeeks/TripletsWithSmallerSum.java
+++ GeeksForGeeks/TripletsWithSmallerSum.java
@@ -1,26 +1,25 @@
-// User function Template for Java
-
 class Solution {
-    long countTriplets(int n, int sum, long arr[]) {
+    int countTriplets(int sum, int arr[]) {
+        // code here
         
+        // sort the array 
         Arrays.sort(arr);
         
-        long count = 0;
+        int ans = 0;
         
-        for(int i=0 ; i< n-2 ; i++){
+        for(int i=0 ; i<arr.length-2 ; i++){
+            int left = i+1,right = arr.length-1;
             
-            int left = i+1 , right = n-1 ;
             while( left < right ){
-                long s = arr[i] + arr[left] + arr[right];
-                if(s < sum){
-                    count+= ( right - left );
-                    left++;
-                }
-                else{
+                int currSum = arr[i] + arr[left] + arr[right];
+                if( currSum >= sum ){
                     right--;
+                }else{
+                    ans = ans + ( right - left );
+                    left++;
                 }
             }
         }
-        return count;
+        return ans;
     }
 }
