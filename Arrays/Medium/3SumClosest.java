dcae3d5d28c2ccd47f1c0527a0daf8613173308d
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-07-16 12:47:57 +0530

    Improve threeSumClosest implementation
    Refactor threeSumClosest method for clarity and efficiency.

--- Arrays/Medium/3SumClosest.java
+++ Arrays/Medium/3SumClosest.java
@@ -1,37 +1,37 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
 
-        //Sort the array
+        // sort the array
         Arrays.sort(nums);
 
-        // to track minimum absolute difference
-        int minDifference = Integer.MAX_VALUE;
-        
-        int resSum = nums[0] + nums[1] + nums[2];
+        int maxDiff = Integer.MAX_VALUE;
+        int resultSum = 0;
 
-        // 3sum logic ( without dupicates )
-        for(int i=0 ; i< nums.length-2 ; i++){
+        for( int i=0 ; i<nums.length-2 ; i++) {
             int left = i+1 , right = nums.length-1;
-            while(left < right){
+            while( left < right ) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == target){
-                    return target;
-                }
-                else if(sum < target){
+
+                if( sum == target ){
+                    resultSum = sum;
                     left++;
-                }else{
+                    right--;
+                    return resultSum;
+                }
+                else if( sum < target ){
+                   left++;
+                }
+                else{
                     right--;
                 }
+                int diff = Math.abs(sum-target);
 
-                // MAIN PART HERE ---
-                int diffToTarget = Math.abs(sum-target);  // CALULATING DIFFERENCE
-                if(diffToTarget < minDifference){         
-                    resSum = sum;                         // UPDATE THE RESULT SUM WITH THE SUM THAT HAS LESS DIFFERENCE WITH TARGET
-                    minDifference = diffToTarget;         // UPDATING DIFFERENCE
+                if( maxDiff > diff ){
+                    maxDiff = diff;
+                    resultSum = sum;
                 }
-
             }
         }
-        return resSum; 
+        return resultSum;
     }
 }
