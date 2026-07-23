9d12ac8df0ead4fb0d5e473e897dee6e6a299177
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-07-23 20:32:01 +0530

    Implement max subarray sum with sliding window
    Added a new implementation for max subarray sum of size k using a sliding window approach.

--- GeeksForGeeks/MaxSumSubarrayOfSizeK.java
+++ GeeksForGeeks/MaxSumSubarrayOfSizeK.java
@@ -2,6 +2,29 @@ class Solution {
     public int maxSubarraySum(int[] arr, int k) {
         // Code here
         int n = arr.length;
+        
+        if( n < k ) return -1;
+
+        int sum = 0 , maxSum = Integer.MIN_VALUE;
+        for(int i=0 ; i<n ; i++){
+            sum += arr[i];
+            
+            if( i >= k ){
+                sum -= arr[i-k];
+            }
+            
+            if( i >= k-1 ){
+               maxSum = Math.max( maxSum , sum) ;
+            }
+        }
+        return maxSum;
+    }
+}
+
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int n = arr.length;
         int low = 0 , high = k-1;
         int res = 0 , sum = 0;
         
