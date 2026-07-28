a67d77e181588bd6866bbd49907413f6ecd60c4a
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-07-28 16:37:18 +0530

    Refactor minWindow method for clarity and efficiency

--- String/Hard/MinWindowSubstring.java
+++ String/Hard/MinWindowSubstring.java
@@ -1,78 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-        if( s == null || t == null || s.length() == 0 || t.length() == 0 ){
-            return "";
+        int n = s.length();
+        int m = t.length();
+
+        if( n < m ) return "";
+
+        int low = 0 , high = 0 , result = Integer.MAX_VALUE , start = 0;
+
+        int[] sFreq = new int[128];
+        int[] tFreq = new int[128];
+
+        for(int i=0 ; i<m ; i++){
+             tFreq[t.charAt(i)]++;
         }
 
-        int left = 0 , right = 0;
-        int start = 0;
-        int req = t.length();
-        int minLen = Integer.MAX_VALUE;
+        for(high = 0 ; high<n ; high++){
+            sFreq[s.charAt(high)]++;
 
-        int freq[] = new int[128];
-        for( char c : t.toCharArray() ){
-            freq[c]++;
-        }
+            while(contains(sFreq,tFreq)){
+                int len = high - low + 1;
 
-        for( right = 0 ; right < s.length() ; right++ ){
-
-            char r = s.charAt(right);
-
-            if( freq[r] > 0 ){
-                req--;
-            }
-            freq[r]--;
-
-            while( req == 0 ){
-
-                if( right - left + 1 < minLen ){
-                    minLen = right - left + 1;
-                    start = left;
+                if (len < result) { 
+                    result = len; start = low; 
                 }
 
-                char l = s.charAt(left);
-                freq[l]++;
-
-                if( freq[l] > 0 ){
-                    req++;
-                }
-
-                left++;
+                sFreq[s.charAt(low)]--;
+                low++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start , start + minLen );
-        
-        // Another Solution
-
-        // if( s == null || t == null || s.length() == 0 || t.length() == 0 ){
-        //     return "";
-        // }
-
-        // int mapS[] = new int[256];
-        // int mapT[] = new int[256];
-
-        // for(char c : t.toCharArray() ) mapT[c]++;
-
-        // int left = 0 , right = 0 , minStart = 0 , minLen = Integer.MAX_VALUE;
-
-        // for(right =0 ; right < s.length() ; right++){
-
-        //     mapS[s.charAt(right)]++;
-
-        //     while( contains( mapS , mapT ) ){
-        //         if( right - left + 1 < minLen ){
-        //             minLen = right - left + 1;
-        //             minStart = left;
-        //         }
-        //         mapS[s.charAt(left++)]--;
-        //     }
-        // }
-        // return minLen == Integer.MAX_VALUE ? "" : s.substring( minStart , minStart + minLen );
+        return result == Integer.MAX_VALUE ? "" : s.substring(start, start + result);
     }
 
     private boolean contains( int[] mapS , int[] mapT ){
-        for( int i=0 ; i<256 ; i++ ){
+        for( int i=0 ; i<128 ; i++ ){
             if( mapT[i] > mapS[i] ){
                 return false;
             }
