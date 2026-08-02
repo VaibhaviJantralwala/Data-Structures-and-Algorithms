e53680b481a9e0876705471a8e7b85b4a7749874
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-08-02 16:10:05 +0530

    Update MaxProdSubarray.java

--- Arrays/Medium/MaxProdSubarray.java
+++ Arrays/Medium/MaxProdSubarray.java
@@ -1,37 +1,23 @@
 class Solution {
-    //O(n)
+    
     public int maxProduct(int[] nums) {
-        //return maxProductBruteForce(nums);
-        
-        int maxProd = nums[0];
-        int minProd = nums[0];
+
+        int minEnding = nums[0];
+        int maxEnding = nums[0];
         int result = nums[0];
 
-        for(int i=1 ; i<nums.length ; i++) {
-            int currElement = nums[i];
-            if( currElement < 0 ){
-                //swap min and max
-                int temp = maxProd;
-                maxProd = minProd;
-                minProd = temp;
-            }
-            maxProd = Math.max( currElement , currElement*maxProd );
-            minProd = Math.min( currElement , currElement*minProd );
-            result = Math.max( maxProd , result );
-        }
-        return result;
-    }
+        for( int i=1 ; i<nums.length ; i++){
 
-    //O(n2)
-    public int maxProductBruteForce(int[] nums) {
-        int maxProd = nums[0];
-        for(int i=0 ; i<nums.length ; i++) {
-            int currProd = 1;
-            for(int j=0 ; j<nums.length ; j++) {
-                currProd *=nums[j];
-                maxProd = Math.max( currProd , maxProd);
-            }
+            int val1 = nums[i];
+            int val2 = minEnding * nums[i];
+            int val3 = maxEnding * nums[i];
+
+            maxEnding = Math.max( val1 , Math.max( val2 , val3 ) );
+            minEnding = Math.min( val1 , Math.min( val2 , val3 ) );
+
+            result = Math.max( result , maxEnding);
         }
-        return maxProd;
+        
+        return result;
     }
 }
