4ddaaf4d0c0c79add6c1f1ce0d55e56c92684787
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-08-11 16:11:53 +0530

    Refactor ValidParenthesis.java for stack implementation

--- Stack/Easy/ValidParenthesis.java
+++ Stack/Easy/ValidParenthesis.java
@@ -1,68 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character,Character> map = new HashMap<>();
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
+       
+       Deque<Character> stk = new ArrayDeque<>();
 
-        Stack<Character> stk = new Stack<>();
+       for( char ch : s.toCharArray() ){
 
-        for(int i=0 ; i<s.length() ; i++) {
-            char c = s.charAt(i);
+            // opening bracket
+            if( ch == '(' || ch == '[' || ch == '{' ){
+                stk.push(ch);
+            }
+            // closing bracket
+            else{
 
-            // A. If the current character is a closing bracket
-            if (map.containsKey(c)) {
+                if( stk.isEmpty() ){
+                    return false;
+                }
 
-            // Condition 1: Check if the stack is empty.
-            // If the stack is empty, it means we have a closing bracket with no unmatched opening bracket.
-            char topElement = stk.empty() ? '#' : stk.pop();
+                char top = stk.pop();
 
-            // Condition 2: Check for a mismatch.
-            // The popped opening bracket must match the current closing bracket.
-                if (topElement != map.get(c)) {
+                if( ( ch == '}' && top != '{' ) || ( ch == ']' && top != '[' ) || ( ch == ')' && top != '(' ) ){
                     return false;
                 }
             }
-            else {
-                // B. If the current character is an opening bracket, push it onto the stack.
-                stk.push(c);
-            }
-        }
-        // 3. Final Check: After iterating through the entire string, the stack must be empty.
-        // If it's not empty (e.g., "(["), it means we have unmatched opening brackets.
-        return stk.empty();
+       }
+
+       return stk.isEmpty();
     }
 }
-
-
-
-
-/*
-import java.util.*;
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
-        for(char ch:s.toCharArray())
-        {
-            if(ch=='{'||ch=='['||ch=='(')
-            {
-                stack.push(ch);
-            }
-            else
-            {
-                if(stack.isEmpty())
-                {
-                    return false;
-                }
-                char top=stack.pop();
-                if((ch=='}' && top!='{' || ch==']' && top!='[' || ch==')' && top!='('))
-                {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-              
-    }
-}
-*/
