17f73f58adafc626e311935a1b008f419bda4265
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-08-13 13:31:32 +0530

    Update NextGreaterEle2.java

--- Stack/Medium/NextGreaterEle2.java
+++ Stack/Medium/NextGreaterEle2.java
@@ -1,29 +1,40 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         
+        // Deque<Integer> stk = new ArrayDeque<>();
+        // int[] result = new int[nums.length];
+        // for( int i=nums.length-2 ; i>=0 ; i-- ){
+        //     stk.push(nums[i]);
+        // }
+        // for( int i=nums.length-1 ; i>=0 ; i-- ){
+        //     while( !stk.isEmpty() && stk.peek() <= nums[i] ){
+        //         stk.pop();
+        //     }
+        //     if( stk.isEmpty() ){
+        //         result[i] = -1;
+        //     }
+        //     else{
+        //         result[i] = stk.peek();
+        //     }
+        //     stk.push(nums[i]);
+        // }
+        // return result;
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans , -1);
+
         Deque<Integer> stk = new ArrayDeque<>();
-        
-        int[] result = new int[nums.length];
-
-        for( int i=nums.length-2 ; i>=0 ; i-- ){
-            stk.push(nums[i]);
-        }
-
-        for( int i=nums.length-1 ; i>=0 ; i-- ){
-
-            while( !stk.isEmpty() && stk.peek() <= nums[i] ){
+        for( int i = 2*n-1 ; i>=0 ; i-- ){
+            int num = nums[i%n];
+            while( !stk.isEmpty() && stk.peek() <= num ){
                 stk.pop();
             }
-
-            if( stk.isEmpty() ){
-                result[i] = -1;
+            if( i<n && !stk.isEmpty() ){
+                ans[i] = stk.peek();
             }
-            else{
-                result[i] = stk.peek();
-            }
-            stk.push(nums[i]);
+            stk.push(num);
         }
-
-        return result;
+        return ans;
     }
 }
