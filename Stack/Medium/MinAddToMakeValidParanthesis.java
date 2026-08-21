911617112c5b579f16ed7f0529a675fac65e9aa0
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-08-21 20:30:02 +0530

    Refactor minAddToMakeValid method using a stack
    Refactored minAddToMakeValid method to use a stack for better readability and efficiency.

--- Stack/Medium/MinAddToMakeValidParanthesis.java
+++ Stack/Medium/MinAddToMakeValidParanthesis.java
@@ -1,6 +1,30 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         
+        int openBrack = 0;
+        int closeBrack = 0;
+
+        for( char ch : s.toCharArray() ){
+
+            if( ch == '(' ){
+                openBrack++;
+            }else{
+                // ch == ')'
+                if( openBrack > 0 ){
+                    openBrack--;
+                }else{
+                    closeBrack++;
+                }
+            }
+        }
+
+        return openBrack + closeBrack;
+    }
+}
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
         Deque<Character> stk = new ArrayDeque<>();
 
         for( char ch : s.toCharArray() ){
