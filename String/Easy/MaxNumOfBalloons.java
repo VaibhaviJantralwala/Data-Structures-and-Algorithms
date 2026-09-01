21643cb122a29fc31ca4077a8668a064f8742808
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-09-01 12:41:08 +0530

    Create MaxNumOfBalloons.java

---
+++ String/Easy/MaxNumOfBalloons.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        
+        int[] freq = new int[26];
+
+        for( char ch : text.toCharArray() ){
+
+            freq[ ch - 'a']++;
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        ans = Math.min( ans , freq['b' - 'a']);
+        ans = Math.min( ans , freq['a' - 'a']);
+        ans = Math.min( ans , freq['l' - 'a'] / 2);
+        ans = Math.min( ans , freq['o' - 'a'] / 2);
+        ans = Math.min( ans , freq['n' - 'a']);
+
+        return ans;
+    }
+}
